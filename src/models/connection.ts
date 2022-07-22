75dd01dfa57e49064a225872c3d8315cce8e4c39
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-07-22 00:01:58 -0300

    refactor: 💡 Fix bug

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -7,7 +7,6 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'us-cdbr-east-06.cleardb.net',
   user: process.env.MYSQL_USER || 'b50c240de048ac',
   password: process.env.MYSQL_PASSWORD || 'e79b0442',
-  database: process.env.MYSQL_DB || 'heroku_562cf739f494a56',
 });
 
 export default connection;
